45b9999e2f245951ed9f1ff65cee9cba55ea8dee
Mohammed Hasan <mohammed@ligado.com>
2024-12-05 10:21:48 -0500

    File Changed

--- Ligado-C++_HelloWorld.cpp
+++ Ligado-C++_HelloWorld.cpp
@@ -1,13 +1,18 @@
 // ********************************************************************
 //  DEVELOPER    : MOHAMMED HASAN
 //  DATE         : 05 DECEMBER 2024
-//  DESCRIPTION  : HELLO WORLD
+//  DESCRIPTION  : Welcome to Ligado - C++ Application
 //  VERSION      : 1.0.0
 // ********************************************************************
 
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main() {
-    std::cout << "Welcome to Ligado - C++ Application Version 1.0.0\n" << std::endl;
+    while (true) {
+        std::cout << "Welcome to Ligado - C++ Application Version 1.0.0\n" << std::endl;
+        std::this_thread::sleep_for(std::chrono::minutes(1));
+    }
     return 0;
 }
